bb7eb3540e9f933febf57e0878ef9ad6111a5186
Hikmat Ullah <hikmatcse1919@gmail.com>
2023-04-17 18:52:06 -0700

    this is the code for resume-template

---
+++ Components/Skills.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className="flex items-center max-w-8xl mx-auto leading-5 mt-10" id="portfolio">
+        <div className="text-gray-500 px-10 mt-30 mx-auto">
+       
+             <h1 className="text-center text-5xl text-white mb-10">
+                Skills
+            </h1>
+            <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-20'>
+                {/* skill1 */}
+                <div className='p-5'>
+                    <img src=""
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
