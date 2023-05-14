176b60942dd28b59983090098c4e4485eb391f83
Hikmat Ullah <hikmatcse1919@gmail.com>
2023-05-14 06:07:41 -0700

    mt

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -15,6 +15,7 @@ const Contact = () => {
              <h1 className="p-24 text-center text-5xl mb-1 text-red-900 font-bold">
                 GET IN TOUCH
             </h1>
+
             <div className='flex flex-row justify-between sm:justify-center sm:flex-col'>
 
                 <div className='flex justify-start flex-col sm:justify-center'>
@@ -40,75 +41,74 @@ const Contact = () => {
             </div>
             
             <div className='flex flex-col px-10 mx-10 sm:basis-0 '>
-             <div className='flex flex-row px-20 mx-0 items-center text-black '>
-                <MdOutlineAlternateEmail className="text-lg w-11 h-11 bg-red-900 rounded-full cursor-pointer hover:scale-105 ease-in duration-300 hover:bg-gray-300 shadow-lg shadow-red-900" />  <div className='mx-10 text-white'>hikmatcse1919@gmail.com</div>
-             </div>
-             <div className='flex flex-row px-20 mx-0 mt-8 items-center text-lg text-black'>
-             <GrLocation  className="text-lg w-11 h-11 bg-red-900 rounded-full cursor-pointer hover:scale-105 ease-in duration-300 hover:bg-gray-300 shadow-lg shadow-red-900" />  <div className='mx-10 text-white'>Peshawar Pakistan</div>
-             </div>
-             <div className='flex flex-row px-20 mx-0 mt-8 items-center text-lg text-black'>
-             <BsWhatsapp  className="text-lg w-11 h-11 bg-red-900 rounded-full cursor-pointer hover:scale-105 ease-in duration-300 hover:bg-gray-300 shadow-lg shadow-red-900" />  <div className='mx-10 text-white'>+923446474475</div>
-             </div>
+               <div className='flex flex-row px-20 mx-0 items-center text-black '>
+                  <MdOutlineAlternateEmail className="text-lg w-11 h-11 bg-red-900 rounded-full cursor-pointer hover:scale-105 ease-in duration-300 hover:bg-gray-300 shadow-lg shadow-red-900" />  <div className='mx-10 text-white'>hikmatcse1919@gmail.com</div>
+               </div>
+               <div className='flex flex-row px-20 mx-0 mt-8 items-center text-lg text-black'>
+                 <GrLocation  className="text-lg w-11 h-11 bg-red-900 rounded-full cursor-pointer hover:scale-105 ease-in duration-300 hover:bg-gray-300 shadow-lg shadow-red-900" />  <div className='mx-10 text-white'>Peshawar Pakistan</div>
+                </div>
+                <div className='flex flex-row px-20 mx-0 mt-8 items-center text-lg text-black'>
+                  <BsWhatsapp  className="text-lg w-11 h-11 bg-red-900 rounded-full cursor-pointer hover:scale-105 ease-in duration-300 hover:bg-gray-300 shadow-lg shadow-red-900" />  <div className='mx-10 text-white'>+923446474475</div>
+                </div>
 
-             <div className="pt-10  items-center mt-5 w-full sm:w-[100%] flex-row justify-around gap-4 px-20 mx-0  text-red-900 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
+                    <div className="pt-10  items-center mt-5 w-full sm:w-[100%] flex-row justify-around gap-4 px-20 mx-0  text-red-900 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
                         <div className='mx-1'>
                             { <Linkedin url="https://www.linkedin.com/in/hikmat-ullah-b284081b9/">
                                 
                                  <FaLinkedin />
 
                             </Linkedin> }
-                            </div>
-                            <div className='mx-1'>
+                        </div>
+                        <div className='mx-1'>
                             <Linkedin url="https://www.facebook.com/hikmat.ullah.1694059">
 
                                 <FaFacebook /> 
 
                             </Linkedin>
-                            </div>
-                            <div className='mx-1'>
+                        </div>
+                        <div className='mx-1'>
                             <Linkedin url="https://www.github.com/Hikmee/">
 
                                 <FaGithub /> 
                                 
                             </Linkedin>
-                            </div>
-                            <div className='mx-1'>
+                        </div>
+                        <div className='mx-1'>
                             <Linkedin url="https://www.twitter.com">
 
                                 <FaTwitter /> 
                                 
                             </Linkedin>
-                            </div>
-                            <div className='mx-1'>
+                        </div>
+                        <div className='mx-1'>
                             <Linkedin url="https://www.youtube.com/@hikmatullah9353">
 
                                 <FaYoutube /> 
                                 
                             </Linkedin>
-                            </div>
-                            <div className='mx-1'>
+                        </div>
+                        <div className='mx-1'>
                             <Linkedin url="https://medium.com/@khanhikmee344">
 
                                 <AiFillMediumSquare /> 
                                 
                             </Linkedin>
-                            </div>
+                        </div>
                             
                             
-                        </div> 
+                    </div> 
+                </div>
 
             </div>
-
-            </div>
-        </div>
-        </div>
-        <div>
-            <p className='flex px-10 justify-text justify-end text-white w-[100%] items-center'>
-                Created by  <a className='text-blue-400 underline' href='https://hikmeedev.vercel.app/'>    Hikmat Ullah</a>
-                </p>
         </div>
     </div>
-  )
+    <div>
+        <p className='flex px-10 justify-text justify-end text-white w-[100%] items-center'>
+            Created by  <a className='text-blue-400 underline' href='https://hikmeedev.vercel.app/'>    Hikmat Ullah</a>
+        </p>
+    </div>
+</div>
+)
 }
 
 export default Contact
