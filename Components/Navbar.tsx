cdd607166b8d4ce2619d14277b8b922b71e122b8
Hikmat Ullah <hikmatcse1919@gmail.com>
2023-05-18 11:49:52 -0700

    text

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -54,7 +54,7 @@ const Navbar = () => {
         </div>
         {/* Mobile Menu*/}
         <div className= {`${nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black" : " "}`}>
-        <div className={`${nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen p-10 ease-in duration-500 bg-white" : "fixed left-[-100%] top-0 p-15 ease-in duration-500 "} `}>
+        <div className={`${nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen p-10 ease-in duration-500 bg-white text-black" : "fixed left-[-100%] top-0 p-15 ease-in duration-500 "} `}>
                 <div className="flex w-full items-center justify-between">
                 <Link href={"/"} >
                     <Image src = {"/Images/logo.png"} 
